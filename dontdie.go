5229570f65881cc954c6f430bcf42609ed69b825
Vladimir Savchenko <vladimir.savchenko@sap.com>
2023-08-18 06:22:50 +0300

    Update dontdie.go

--- dontdie.go
+++ dontdie.go
@@ -2,34 +2,43 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 )
 
-func main() {
-
-	log.Printf("Started server with pid %d\n", os.Getpid())
-
-	signal.Ignore(syscall.SIGTERM)
-
-	port, ok := os.LookupEnv("PORT")
-	if !ok {
-		port = "8080"
-	}
-
+func handler(w http.ResponseWriter, r *http.Request) {
 	instance, ok := os.LookupEnv("CF_INSTANCE_INDEX")
 	if !ok {
 		instance = "UNKNOWN"
 	}
+	_, _ = w.Write([]byte(fmt.Sprintf("This could be a meaningful HTTP response coming from instance %s\n", instance)))
+}
 
-	mux := http.NewServeMux()
+func main() {
+	// Setting up a basic HTTP server
+	http.HandleFunc("/", handler)
+	go func() {
+		if err := http.ListenAndServe(":8080", nil); err != nil && err != http.ErrServerClosed {
+			fmt.Println("Server error:", err)
+		}
+	}()
 
-	mux.HandleFunc("/", func(writer http.ResponseWriter, _ *http.Request) {
-		_, _ = writer.Write([]byte(fmt.Sprintf("This could be a meaningful HTTP response coming from instance %s\n", instance)))
-	})
+	// Setting up signal catching
+	sigCh := make(chan os.Signal, 1)
 
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), mux))
+	// Catching SIGTERM
+	signal.Notify(sigCh, syscall.SIGTERM)
+
+	// Waiting for SIGTERM
+	sig := <-sigCh
+	if sig == syscall.SIGTERM {
+		fmt.Println("SIGTERM received. Waiting for 5 minutes...")
+
+		// Waiting for 5 minutes
+		time.Sleep(5 * time.Minute)
+		fmt.Println("5 minutes elapsed, shutting down.")
+	}
 }
